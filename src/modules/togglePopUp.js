31d7a5ab2bc5262966dccf7fbc2315e269a35f64
Andrew <andrew79sf@gmail.com>
2019-10-08 09:03:11 -0700

    add Gift Modal Window

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -3,18 +3,26 @@ function togglePopUp() {
     const popupList = document.querySelectorAll('.popup'),
       target = event.target,
       popupId = target.getAttribute('data-popup'),
-      popup = document.querySelector(popupId);
-
+      popup = document.querySelector(popupId),
+			giftIcon = document.querySelector('.fixed-gift');
+			
     if (popupId == '#free_visit_form' || popupId == '#callback_form') {
       popup.style.display = 'block';
     }
 
-    if (target.matches('.overlay') || target.matches('.close_icon') || target.matches('img')) {
+    if (target.matches('.overlay') || target.matches('.close_icon') || target.matches('.close-btn')) {
       popupList.forEach((elem) => {
         if (window.getComputedStyle(elem).display == 'block') {
-          elem.style.display = 'none';
-        }
+					elem.style.display = 'none';
+					giftIcon.style.display = 'block';
+				}
       });
+    }
+
+    if (target.closest('.fixed-gift')) {
+      const popup = document.getElementById('gift');
+      popup.style.display = 'block';
+      giftIcon.style.display = 'none';
     }
   });
 }
